15fb8d9ab3edbcbf09ee1817fa6b747eea7c939b
Leo Dion <leogdion@brightdigit.com>
2021-07-07 19:47:46 -0400

    Initial Commit

---
+++ Toodles/ToodlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToodlesApp.swift
+//  Toodles
+//
+//  Created by Leo Dion on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ToodlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
