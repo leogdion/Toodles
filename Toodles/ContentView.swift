63280a795c1fe98c887f744bfe691bda651baeda
Leo Dion <leogdion@brightdigit.com>
2021-07-07 21:31:14 -0400

    adding assets and UI

--- Toodles/ContentView.swift
+++ Toodles/ContentView.swift
@@ -7,10 +7,45 @@
 
 import SwiftUI
 
+struct IceCreamImage : Identifiable {
+  let id : String
+}
 struct ContentView: View {
+  @State var selectedIceCreams = Set<String>()
+  var images : [IceCreamImage] {
+    names.components(separatedBy: .newlines).compactMap{ word in
+      let word = word.trimmingCharacters(in: .whitespacesAndNewlines)
+      guard word.count > 4 else {
+        return nil
+      }
+      let index = word.index(word.endIndex, offsetBy: -5)
+      let result =  word[word.startIndex...index]
+      return String(result)
+    }.map(
+      IceCreamImage.init
+    )
+  }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      ScrollView {
+      LazyVGrid(columns: [GridItem(),GridItem(),GridItem()]){
+        ForEach(self.images) { iceCream in
+          Button {
+            if selectedIceCreams.contains(iceCream.id) {
+              selectedIceCreams.remove(iceCream.id)
+            } else {
+              selectedIceCreams.update(with: iceCream.id)
+            }
+          } label: {
+            ZStack{
+              RoundedRectangle(cornerRadius: 4.0, style: .circular).foregroundColor(.primary).opacity(selectedIceCreams.contains(iceCream.id) ? 0.1 : 0.2)
+              Image(iceCream.id).resizable().scaledToFit().padding(10.0).opacity(selectedIceCreams.contains(iceCream.id) ? 1.0 : 0.8)
+            }.padding(4.0)
+          }.padding(.horizontal, 4.0)
+
+          
+        }
+      }
+    }
     }
 }
 
---
+++ Toodles/Names.swift
@@ -0,0 +1,52 @@
+let names = """
+  001-ice cream.svg
+  002-popsicle.svg
+  003-popsicle.svg
+  004-popsicle.svg
+  005-popsicle.svg
+  006-ice cream.svg
+  007-ice cream.svg
+  008-ice cream.svg
+  009-ice cream.svg
+  010-crepe.svg
+  011-ice cream.svg
+  012-ice cream.svg
+  013-ice cream.svg
+  014-bucket.svg
+  015-smoothie.svg
+  016-ice cream.svg
+  017-ice cream.svg
+  018-cake.svg
+  019-pancake.svg
+  020-ice cream sandwich.svg
+  021-ice cream.svg
+  022-ice cream.svg
+  023-taiyaki.svg
+  024-cups.svg
+  025-cone.svg
+  026-shop.svg
+  027-kiosk.svg
+  028-food stand.svg
+  029-ice cream truck.svg
+  030-freezer.svg
+  031-ice cream machine.svg
+  032-refrigerator.svg
+  033-menu.svg
+  034-signage.svg
+  035-signage.svg
+  036-apron.svg
+  037-location.svg
+  038-jam.svg
+  039-syrup.svg
+  040-macaron.svg
+  041-scoop.svg
+  042-flavour.svg
+  043-spoon.svg
+  044-shopkeeper.svg
+  045-shopkeeper.svg
+  046-packaging.svg
+  047-strawberry.svg
+  048-cherry.svg
+  049-wafer.svg
+  050-chocolate chip.svg
+"""
